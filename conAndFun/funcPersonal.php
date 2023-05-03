291c26844e099a86809dee174eca4cf264517eb6
AdeRianos <132426190+AdeRianos@users.noreply.github.com>
2023-05-03 11:43:37 +0330

    Delete funcPersonal.php

--- conAndFun/funcPersonal.php
+++
@@ -1,8 +0,0 @@
-<?php
-function validate_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    $data = filter_var($data);
-    return $data;
-}

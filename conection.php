8029c9eda0cea636ffd56294281e448bb58ba0e3
AdeRianos <132426190+AdeRianos@users.noreply.github.com>
2023-05-03 12:16:12 +0330

    Add files via upload

--- conection.php
+++ conection.php
@@ -1,27 +1,17 @@
 <?php
-class mypdoApp
-{
-    public $localhost;
-    public $dbname;
-    public $username;
-    public $password;
-    function myldup()
-    {
-        $this->localhost = 'localhost';
-        $this->dbname = 'school';
-        $this->user = 'root';
-        $this->pass = '';
-    }
-}
-$datConection = new mypdoApp;
-$datConection->myldup();
-//test($datConection);
-try {
-    $appConection = new PDO("mysql:local=$datConection->localhost;dbname=$datConection->dbname", $datConection->user, $datConection->pass);
-    $appConection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $resCon= $appConection->query('SET NAMES utf8');
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
-?>
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "school";
 
+// ایجاد اتصال به دیتابیس با استفاده از کلاس PDO
+try {
+$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+// تنظیم ویژگی های PDO
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+echo "اتصال به دیتابیس با موفقیت برقرار شد.";
+} catch(PDOException $e) {
+echo "خطا در اتصال به دیتابیس: " . $e->getMessage();
+}
